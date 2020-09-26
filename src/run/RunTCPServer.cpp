0a3809ff8a78ac013978d7dcf09d01037def8a32
gaertnerl <gaertnerleonard@web.de>
2020-09-26 16:09:18 +0200

    Created a simple program to start up the tcp server.

---
+++ src/run/RunTCPServer.cpp
@@ -0,0 +1,11 @@
+//
+// Created by gaertnerl on 21.09.20.
+//
+
+#include "../TCPServer/tcpServer.h"
+
+int main(int argc, char const *argv[])
+{
+    moebius::TCPServer tcpServer = moebius::TCPServer();
+    return 0;
+}
