f3927b7699f3fa0a48b7a05e64b5312aec7ec675
gaertnerl <gaertnerleonard@web.de>
2020-09-26 16:09:26 +0200

    Created a simple program to start up a tcp client.

---
+++ src/run/RunTCPClient.cpp
@@ -0,0 +1,11 @@
+//
+// Created by gaertnerl on 24.09.20.
+//
+
+#include "../TCPClient/tcpClient.h"
+
+int main(int argc, char const *argv[])
+{
+    moebius::TCPClient tcpClient = moebius::TCPClient();
+    return 0;
+}
