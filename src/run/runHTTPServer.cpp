b145e935cda4f1f15b4bdbd0df82dd805cf928f3
gaertnerl <gaertnerleonard@web.de>
2021-01-01 15:28:30 +0100

    added runner for http server

---
+++ src/run/runHTTPServer.cpp
@@ -0,0 +1,12 @@
+//
+// Created by gaertnerl on 01.11.20.
+//
+
+#include "../HTTPServer/HTTPServer.h"
+#include "../TCPServer/tcpServer.h"
+
+int main(int argc, char const *argv[])
+{
+    moebius::HTTPServer httpServer;
+    return 0;
+}
